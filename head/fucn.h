30f29d2706435c94c0b3191cf10d9b4f7fa9af6a
ilysha_wenn <81334200+ilysha-wenn@users.noreply.github.com>
2021-03-25 02:25:24 +0300

    proj_1

---
+++ head/fucn.h
@@ -0,0 +1,5 @@
+#ifndef FUNC_H_
+#define FUNC_H_
+
+extern int ur(int a , int b);
+#endif
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "fucn.h"
+
+
+int main(){
+    int gs, a = 2,
+        b = 3;
+
+    printf("%d",ur(a,b));
+    scanf("%d", &gs);
+    return 0;
+}
+
+
